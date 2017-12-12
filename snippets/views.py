ae946528d6e8fe9920ecf761af229581aea9f668
Sameeran Kunche <skunche@gmail.com>
2017-12-11 21:13:14 -0800

    Add optional format suffixes to the URLs

--- snippets/views.py
+++ snippets/views.py
@@ -6,7 +6,7 @@ from snippets.serializers import SnippetSerializer
 
 
 @api_view(['GET', 'POST'])
-def snippet_list(request):
+def snippet_list(request, format=None):
     """
     List all code snippets, or create a new snippet
     :param request:
@@ -25,8 +25,8 @@ def snippet_list(request):
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
-@csrf_exempt
-def snippet_detail(request, pk):
+@api_view(['GET', 'PUT', 'DELETE'])
+def snippet_detail(request, pk, format=None):
     """
     Retrieve, update or delete a code snippet
     :param request:
